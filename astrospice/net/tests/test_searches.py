f17910cd5c6edee7e93c95c0841d04424c6ebe45
David Stansby <dstansby@gmail.com>
2021-11-07 19:19:12 +0000

    Fix tests

--- astrospice/net/tests/test_searches.py
+++ astrospice/net/tests/test_searches.py
@@ -1,16 +1,12 @@
 import pytest
 
+from astropy.table import Table
+
 from astrospice import registry as spice_reg
 from astrospice.net import RemoteKernel
 
 
 @pytest.mark.parametrize('name', spice_reg.names)
-@pytest.mark.parametrize('type', ['predict', 'recon'])
-def test_searches(name, type):
-    if type not in spice_reg[name]:
-        pytest.skip(f'{name} has no registry entry for type {type}')
-
-    kernels = spice_reg.get_remote_kernels(name, type)
-    assert isinstance(kernels, list)
-    for k in kernels:
-        assert isinstance(k, RemoteKernel)
+def test_search(name):
+    kernels = spice_reg.get_available_kernels(name)
+    assert isinstance(kernels, Table)
