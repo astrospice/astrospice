9849d8374e741c836c31e0da06299dbcf75c0e64
David Stansby <dstansby@gmail.com>
2021-12-06 11:50:50 +0000

    Unskip STEREO tests

--- astrospice/tests/test_registry.py
+++ astrospice/tests/test_registry.py
@@ -9,7 +9,5 @@ pytestmark = pytest.mark.filterwarnings(r'ignore:ERFA function.*dubious year')
 
 @pytest.mark.parametrize('body', registry.bodies)
 def test_search(body):
-    if 'st' in body:
-        pytest.xfail('STEREO servers not working')
     kernels = registry.get_available_kernels(body)
     assert isinstance(kernels, Table)
