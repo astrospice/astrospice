bc8edecbe2468df8f06364b8b1dd3142845cbfa1
David Stansby <dstansby@gmail.com>
2021-12-06 16:16:45 +0000

    Revert "Manually set version (REVERT ME)"
    This reverts commit a88e7543eb58b10536b737196e4cfd029b00e7f0.

--- astrospice/version.py
+++ astrospice/version.py
@@ -1,9 +1,8 @@
 # Note that we need to fall back to the hard-coded version if either
 # setuptools_scm can't be imported or setuptools_scm can't determine the
 # version, so we catch the generic 'Exception'.
-__version__ = '0.0.1'
-'''try:
+try:
     from setuptools_scm import get_version
     __version__ = get_version(root='..', relative_to=__file__)
 except Exception:
-    __version__ = '0.1.dev76+g036d993''''
+    __version__ = '0.1.dev1+g701c2b1'
