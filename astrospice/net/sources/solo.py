9aaad9530af2aeb1124fd4c0c4a01359f7b50685
Ronan Laker <ronan.laker@gmail.com>
2022-01-13 18:55:57 +0000

    created metakernel class and loaded the urls

--- astrospice/net/sources/solo.py
+++ astrospice/net/sources/solo.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 from astropy.time import Time
 from bs4 import BeautifulSoup
 
-from astrospice.net.reg import RemoteKernel, RemoteKernelsBase
+from astrospice.net.reg import RemoteKernel, RemoteKernelsBase, MetaKernel
 
 __all__ = ['SolarOrbiterPredict']
 
@@ -60,3 +60,76 @@ class SolarOrbiterPredict(RemoteKernelsBase):
         end_time = Time.strptime(times[1], '%Y%m%d')
         version = int(fname[4][1:])
         return True, start_time, end_time, version
+
+class SolarOrbiterMetakernel(RemoteKernelsBase):
+    """Will load the metakernel and all the subsequent kernels listed.
+    
+    To start with I will just give it a local path to the metakernel.
+    
+    """
+    
+    body = 'solar orbiter'
+    type = 'meta'
+    
+    def get_remote_kernels(self):
+        #get the url of the remote meta kernel and download it
+        """
+        Returns
+        -------
+        list[RemoteKernel]
+        """
+        base_url = 'http://spiftp.esac.esa.int/data/SPICE/SOLAR-ORBITER/kernels/mk'
+        page = urlopen(base_url)
+        soup = BeautifulSoup(page, 'html.parser')
+
+        kernel_urls = []
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if href is not None and href.endswith('.tm'):
+                fname = href.split('/')[-1]
+                matches = self.matches(fname)
+                if matches:
+                    kernel_urls.append(
+                        MetaKernel(f'{base_url}/{fname}', *matches[1:]))
+
+        return kernel_urls
+
+    @staticmethod
+    def matches(fname):
+        """
+        Check if the given filename matches the pattern of this kernel.
+
+        Returns
+        -------
+        matches : bool
+        start_time : astropy.time.Time
+        end_time : astropy.time.Time
+        version : int
+        """
+        # Example filename: solo_ANC_soc-flown-mk_V106_20201216_001.tm
+        fname = fname.split('_')
+        if (len(fname) != 6 or
+                fname[0] != 'solo' or
+                fname[1] != 'ANC' or
+                fname[2] != 'soc-flown-mk' and
+                fname[2] != 'soc-pred-mk'):
+            return False
+
+        time = fname[4]
+        time = Time.strptime(time, '%Y%m%d')
+        version = int(fname[3][1:])
+        return True, time, version
+
+        
+        #change the kernel attribute in the metakernel file
+        
+        #cycle through the listed files in the metakernel and download them
+        
+        #these have the same https address as the file system for my local copy
+        #so save in this structure too
+        
+        #load as KernelBase objets
+        
+        #if .spk then load as one of those objects
+        print('Downloading metakernel file')
+    
