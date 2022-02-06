ac2edaf660a02ed599df371b78369c23728966af
Ronan Laker <ronan.laker@gmail.com>
2022-02-06 19:21:58 +0000

    added init test and use fixtures

--- astrospice/tests/test_kernel.py
+++ astrospice/tests/test_kernel.py
@@ -10,16 +10,28 @@ from astrospice.kernel import MetaKernel
 METAKERNEL_CONTENT = "KERNELS_TO_LOAD   = (\n                           '$KERNELS/test_subfolder/test_kernel.bsp'\n                         )"
 
 
+@pytest.fixture()
+def example_mk(tmp_path):
+    fname = tmp_path / "temp_mk.tm"
+    fname.write_text(METAKERNEL_CONTENT)
+    return fname
+
+
+def create_example_kernel(tmp_path):
+    extra_kernel = tmp_path / 'test_subfolder' / 'test_kernel.bsp'
+    extra_kernel.parent.mkdir()
+    extra_kernel.write_text(" ")
+    return extra_kernel
+
+
 def test_unkown_extension(tmp_path):
     fpath = tmp_path / 'test.xyz'
     with pytest.raises(ValueError, match='Filename extension ".xyz" not in'):
         Kernel(fpath)
 
 
-def test_metakernel_kernels(tmp_path):
-    fname = tmp_path / "temp_mk.tm"
-    fname.write_text(METAKERNEL_CONTENT)
-    mk = MetaKernel(fname)
+def test_metakernel_kernels(example_mk):
+    mk = MetaKernel(example_mk)
     kernels = mk.kernels
     assert isinstance(kernels, list)
     assert isinstance(kernels[0], Path)
@@ -27,26 +39,23 @@ def test_metakernel_kernels(tmp_path):
     assert kernels[0].parent.stem == 'test_subfolder'
 
 
-def test_kernels_exist(tmp_path):
-    fname = tmp_path / "temp_mk.tm"
-    fname.write_text(METAKERNEL_CONTENT)
-    mk = MetaKernel(fname)
+def test_kernels_exist(example_mk, tmp_path):
+    mk = MetaKernel(example_mk)
     assert not mk.kernels_exist
-    # now add the file and check
-    extra_kernel = tmp_path / 'test_subfolder' / 'test_kernel.bsp'
-    extra_kernel.parent.mkdir()
-    extra_kernel.write_text(" ")
+    create_example_kernel(tmp_path)
     assert mk.kernels_exist
 
 
-def test_metakernels_load(tmp_path):
-    fname = tmp_path / "temp_mk.tm"
-    fname.write_text(METAKERNEL_CONTENT)
-    mk = MetaKernel(fname)
-    extra_kernel = tmp_path / 'test_subfolder' / 'test_kernel.bsp'
-    extra_kernel.parent.mkdir()
-    extra_kernel.write_text(" ")
+def test_metakernels_load(example_mk, tmp_path):
+    mk = MetaKernel(example_mk)
+    create_example_kernel(tmp_path)
     # should get a SpiceFILEREADFAILED error
     with pytest.raises(Exception) as exc:
         mk.load_kernels()
+        assert isinstance(exc.value, SpiceFILEREADERROR)
+
+
+def test_metekernels_init(example_mk):
+    with pytest.raises(Exception) as exc:
+        MetaKernel(example_mk)
         assert isinstance(exc.value, SpiceFILEREADERROR)
