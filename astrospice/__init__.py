3cac72d1d318d2fb2c8184e6f06ba383b0e5c3e0
David Stansby <dstansby@gmail.com>
2022-09-29 21:58:30 +0100

    Make pre-commit happy

--- astrospice/__init__.py
+++ astrospice/__init__.py
@@ -3,11 +3,11 @@
 import astrospice.time
 from astrospice.net.generic import *
 from astrospice.net.generic import _setup_generic_files
+from ._version import __version__
 from .body import *
 from .config import *
 from .coords import *
 from .kernel import *
 from .net import registry
-from ._version import __version__
 
 _setup_generic_files()
