99a0c21bc4671f2bc177b8d65676db287556fa5b
Ronan Laker <ronan.laker@gmail.com>
2022-10-05 09:30:55 +0100

    Took out line that stopped splitting up content
    This will now not be necessary to work with the new parfive update

--- astrospice/net/reg.py
+++ astrospice/net/reg.py
@@ -4,17 +4,11 @@
 A registry of SPICE kernels for various missions.
 """
 import abc
-import os
 from collections import defaultdict
 from dataclasses import dataclass
 
 import astropy.time
 
-# disable splitting the download since psp predict kernel server does not give a content-length in the response
-# this will not be necessary if parfive is changed
-
-
-os.environ["PARFIVE_DISABLE_RANGE"] = "1"
 import parfive  # noqa: E402
 from astropy.table import Table, vstack  # noqa: E402
 from astropy.time import Time  # noqa: E402
